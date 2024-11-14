febe2273d01c9554745b9d5071cb19192612a38a
Ayesom <167323658+Ayesom@users.noreply.github.com>
2024-11-14 19:44:14 +0530

    Add files via upload

---
+++ include/index.php
@@ -0,0 +1,49 @@
+<?php
+include("include/header.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+</head>
+<body>
+<?php include ("C:\wamp64\www\hms\include\header.php"); ?>
+<div style="margin-top: 50px"></div>
+
+<div class="container">
+	<div class= "col-md-12">
+		<div class="row">
+			<div class = "col-md-3 mx-1 shadow">
+				<img src="img/info.png" style="width: 100%; height: 190px;">
+				<h5 class="text-center">Click on the button for more information.</h5>
+				<a href="#">
+					<button class="btn btn-success my-3" style="margin-left: 20%;">More Information</button>
+				</a>
+            </div>
+
+			<div class = "col-md-4 mx-1 shadow">
+				<img src="img/patient" style="width: 100%; height: 190px;" >
+				<h5 class="text-center">Create account so that we can take good care of you. </h5>
+				<a href="#">
+					<button class="btn btn-success my-3" style="margin-left: 30%;">Create Account!!!</button>
+				</a>
+			</div>
+			<div class = "col-md-4 mx-1 shadow">
+				<img src="img/doctor"style="width: 100%; height: 200px;">
+
+				<h5 class="text-center">We are employing for doctors</h5>
+				<a href="#">
+					<button class="btn btn-success my-3" style="margin-left: 30%;">Apply now!!!</button>
+				</a>
+			</div>
+			
+			
+			
+		</div>
+		
+
+	</div>
+	
+</div>
+</body>
+</html>
